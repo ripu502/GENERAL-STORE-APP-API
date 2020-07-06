a97f26c66d3e00670bd78b765e8b35fa98dcac96
Ripudaman Singh <ripu502@gmail.com>
2020-07-06 18:38:52 +0530

    basic app.js is ready

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
+
+const app = express();
+
+const router = require('./router/router');
+
+const port = process.env.PORT || 3000;
+app.use(helmet());
+app.use(compression())
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.use(router);
+
+mongoose.connect('some mongo uri',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(res => {
+        console.log('connected');
+        app.listen(port, () => {
+            console.log(`server is running at http://localhost:port`);
+        })
+    })
+    .catch(err => {
+        console.log(`some err occured err`);
+    })
